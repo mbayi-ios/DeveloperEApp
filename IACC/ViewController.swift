71867b5d6759db6a70b6b864dcfbde026161ed7c
Ambrose <ambymbayi45@gmail.com>
2023-10-02 08:18:18 +0300

    Initial Commit

---
+++ IACC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IACC
+//
+//  Created by Ambrose Mbayi on 02/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
